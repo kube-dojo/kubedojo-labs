31f919b582a763fd18512352e6b947c280386f37
Krisztian oos <krisztian.koos@gmail.com>
2026-09-11 21:11:01 +0200

    fix(ckad-3): replace /root/ with $HOME/ in solution.sh
    Ref: #15
    Ref: #2272
    Ref: #2276

--- ckad-3.2-logging/step2/solution.sh
+++ ckad-3.2-logging/step2/solution.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl logs log-generator -n logging-lab -c sidecar --tail=5 > /root/sidecar-logs.txt
-kubectl get pod log-generator -n logging-lab -o jsonpath='{.spec.containers[*].name}' > /root/container-names.txt
+kubectl logs log-generator -n logging-lab -c sidecar --tail=5 > "$HOME"/sidecar-logs.txt
+kubectl get pod log-generator -n logging-lab -o jsonpath='{.spec.containers[*].name}' > "$HOME"/container-names.txt
