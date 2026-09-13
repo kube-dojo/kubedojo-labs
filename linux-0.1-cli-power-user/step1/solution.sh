943e30c37cd487e9a0e21c2d0c06d276a8de36f7
Krisztian oos <krisztian.koos@gmail.com>
2026-09-13 16:43:12 +0200

    fix(linux-0.1): enable ll alias in non-interactive solution (#92)

--- linux-0.1-cli-power-user/step1/solution.sh
+++ linux-0.1-cli-power-user/step1/solution.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Solution: Add ll alias to ~/.bashrc
 echo "alias ll='ls -la'" >> ~/.bashrc
-source ~/.bashrc
+# Non-interactive bash skips ~/.bashrc and does not expand aliases.
+shopt -s expand_aliases
+alias ll='ls -la'
 ll
